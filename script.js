1b1c5c9e0bf62c5d20a22c9a7de6794cc2de2e8e
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2025-03-23 16:40:29 +0545

    updated

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const listContainer = document.getElementById('list-container')
 
 function addTask(){
     if(inputBox.value === ''){
-        alert('You must write something!')
+    alert('You must write something!')
     }else{
 let li = document.createElement('li');
 li.innerHTML = inputBox.value;
